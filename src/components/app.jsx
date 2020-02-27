0a56f17c6955cf9203e89015fe1b6549febbee5a
IB_Student <student@InnovateBhams-MacBook-Pro-4.local>
2020-02-27 15:53:16 -0600

    second commit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,14 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Albums from "./Albums";
 import Comments from "./comments";
 import Users from "./users";
 
-class App extends Component {
-  render() {
+function App() {
+  {
     return (
       <Router>
-        <Fragment>
           <div id="buttons" style={{display: "flex", justifyContent: "center", margin: "20px", padding: "20px"}}>
             <Link to="/">
               <button type="button" className="btn btn-outline-dark">
@@ -41,7 +40,6 @@ class App extends Component {
             <Route path="/comments/" component={Comments} />
             <Route path="/Albums/" component={Albums} />
           </Switch>
-        </Fragment>
       </Router>
     );
   }
